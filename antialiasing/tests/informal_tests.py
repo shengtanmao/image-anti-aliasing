7dea68daefea66a7263bc03b04ba0b1b9af45499
smao <maoshengtan2011@gmail.com>
2020-10-13 21:54:47 -0400

    removed informal_tests.py

--- antialiasing/tests/informal_tests.py
+++
@@ -1,9 +0,0 @@
-from src.image_handling import *
-from src.supersampling.uniform_grid import *
-import pathlib as pl
-
-images_path = pl.Path('./antialiasing/tests/images')
-
-image=imp(images_path / "2017-Rolls-Royce-Wraith-012-2160.jpg")
-modified=unif_grid(image,2)
-exp(modified,images_path / "rr.jpg",)
